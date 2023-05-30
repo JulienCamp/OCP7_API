48a7cff0593907a579db02404e4d07484f1641f9
Julien Campagnaud <julien.campagnaud@gmail.com>
2023-05-30 21:57:41 +0200

    commented computing of shap values

--- flask_api.py
+++ flask_api.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 import joblib
 import pandas as pd
-import shap
+#import shap
 
 app = Flask(__name__)
 
@@ -12,23 +12,23 @@ model = joblib.load(MODELS_PATH + "best_model.joblib")
 cal_model = joblib.load(MODELS_PATH + "cal_best_model.joblib")
 data = joblib.load (DATA_PATH + "test_data.joblib")
 
-def get_shap(client_data) :
+# def get_shap(client_data) :
 
-    explainer = shap.TreeExplainer(model['classifier'])
-    # Get the feature names (lost during scaling)
-    column_transformer = model.named_steps['transformers']
-    feature_names = column_transformer.get_feature_names_out()
+#     #explainer = shap.TreeExplainer(model['classifier'])
+#     # Get the feature names (lost during scaling)
+#     column_transformer = model.named_steps['transformers']
+#     feature_names = column_transformer.get_feature_names_out()
     
-    # create preprocessed array from the line of dataframe which corresponds to the client
-    X_t = model['transformers'].transform(client_data)
-    # create preprocessed dataframe from preprocessed array
-    X_t_df = pd.DataFrame(X_t, columns=feature_names)
-    chosen_instance = X_t_df
-    shap_values = explainer.shap_values(chosen_instance)
-    shap_values_list = [arr.tolist() for arr in shap_values]
+#     # create preprocessed array from the line of dataframe which corresponds to the client
+#     X_t = model['transformers'].transform(client_data)
+#     # create preprocessed dataframe from preprocessed array
+#     X_t_df = pd.DataFrame(X_t, columns=feature_names)
+#     chosen_instance = X_t_df
+#     shap_values = explainer.shap_values(chosen_instance)
+#     shap_values_list = [arr.tolist() for arr in shap_values]
 
     
-    return shap_values_list
+#     return shap_values_list
 
 @app.route('/api/prediction', methods=['POST'])
 def prediction():
@@ -41,17 +41,17 @@ def prediction():
     if model_type == "default" :
         prediction = model.predict(client_data)
         confidence = model.predict_proba(client_data)
-        shap_values = get_shap(client_data)
+#        shap_values = get_shap(client_data)
     else :
         prediction = cal_model.predict(client_data)
         confidence = cal_model.predict_proba(client_data)
-        shap_values = get_shap(client_data)
+#       shap_values = get_shap(client_data)
     print("dbg3")
     print("prediction=" , prediction[0])
     # Renvoyer la prédiction en tant que réponse JSON
     return jsonify({'prediction': str(prediction[0]),
                     'confidence' : str(confidence[0][1]),
-                    'shap_values' : shap_values
+                    #'shap_values' : shap_values
                     })
     
 if __name__ == '__main__':
